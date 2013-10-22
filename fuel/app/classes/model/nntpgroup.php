4c5858524c988be0a2419249c97332f73b296452
Mark Heath (WXB035881PR830) <mark@silicontrip.org>
2013-10-23 09:43:03 +1100

    Added groups query to nntpgroup module.

--- fuel/app/classes/model/nntpgroup.php
+++ fuel/app/classes/model/nntpgroup.php
@@ -16,4 +16,14 @@ class Model_Nntpgroup extends \Model
 	);
 
 
+	public static getAllActive() {
+
+		$query = DB::select('hostname','port','ssl','username','password','name','current_article')->from('NNTPGroup');
+		$query->join('NNTPServer');
+		$query->on('NNTPGroup.NNTPServer_id', '=', 'NNTPServer.id');
+		$query->where('NNTPGroup.active', '=', '1');
+		$query->and_where('NNTPGroup.active', '=', '1');
+		return $query->execute()->as_array();
+	}
+
 }
