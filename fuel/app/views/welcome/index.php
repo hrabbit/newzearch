da1156732e4c95ef455141cb376456fe316032dc
Scott <scott@horsley.id.au>
2013-10-26 01:14:21 +1100

    Added pagination to view

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -1,5 +1,13 @@
-<?php if(count($articles)): ?>
-
+<?php if(count($articles) || 1 == 1): ?>
+<table class="table table-bordered table-striped table-condensed">
+<thead>
+	<tr>
+		<th>Title</th>
+		<th>Time</td>
+	</tr>
+</thead>
+</table>
+<?=Pagination::instance('mypagination')->first()?> <?=Pagination::instance('mypagination')->previous()?> <?=Pagination::instance('mypagination')->render()?> <?=Pagination::instance('mypagination')->next()?> <?=Pagination::instance('mypagination')->last() ?>
 <?php else: ?>
 	No articles available
 <?php endif ?>
