fc724a8c2fbf392a63c612656b2950659ebe3ba5
Scott <scott@horsley.id.au>
2013-10-26 00:18:42 +1100

    Added a REST based RSS controller

---
+++ fuel/app/classes/controller/rss.php
@@ -0,0 +1,24 @@
+<?php
+
+class Controller_RSS extends Controller_Rest
+{
+    // Get a users RSS feed based on their search criteria
+    // Requires a valid account
+    public function get_user($md5 = string)
+    {
+
+    }
+
+    // Get every item from the feed as they arrive
+    // Requires a valid account
+    public function get_all($md5 = string)
+    {
+
+    }
+
+    // Get site news
+    public function get_news()
+    {
+
+    }
+}
