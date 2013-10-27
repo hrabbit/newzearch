f456899949e5fef218f54df9a82412e32038a9f9
Scott <scott@horsley.id.au>
2013-10-27 13:32:11 +1100

    Added active and server_id columns

--- fuel/app/migrations/002_create_nntpgroups.php
+++ fuel/app/migrations/002_create_nntpgroups.php
@@ -8,10 +8,12 @@ class Create_nntpgroups
 	{
 		\DBUtil::create_table('nntpgroups', array(
 			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'nntpserver_id' => array('constraint' => 11, 'type' => 'int'),
 			'message_id' => array('constraint' => 255, 'type' => 'varchar'),
 			'title' => array('constraint' => 255, 'type' => 'varchar'),
 			'creation_time' => array('type' => 'datetime'),
 			'description' => array('type' => 'text'),
+			'active' => array('type' => 'boolean'),
 			'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
 			'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
 
