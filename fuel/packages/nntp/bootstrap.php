4ba430dbfcc9b0d3b8b3913bbcc4b45a1e3c4fe4
Scott <scott@horsley.id.au>
2013-10-27 15:27:18 +1100

    Added Net/NNTP package, tweaked to suite Fuel

---
+++ fuel/packages/nntp/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Fuel
+ *
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+
+Autoloader::add_classes(array(
+    'Net_NNTP_Client' => __DIR__.'/Net/NNTP/Client.php',
+    'Net_NNTP_Protocol_Client' => __DIR__.'/Net/NNTP/Protocol/Client.php',
+    'Net_NNTP_Protocol_ResponseCode' => __DIR__.'/Net/NNTP/Protocol/Responsecode.php',
+
+));
+
+/* End of file bootstrap.php */
+
